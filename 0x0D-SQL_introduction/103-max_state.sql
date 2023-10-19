0adc78b76fddf8bdff9a6c0782fd025c4e212d05
kennan12 <kenanusheta24@gmail.com>
2023-10-19 09:38:37 -0700

    12-no_cheating.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Displays the max temprature of each state ordered by ststename
--- source temprature.sql;
+-- source tempratures.sql;
 SELECT state, MAX(value) AS max_temp
 FROM tempratures
 GROUP BY state
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
--- update the score of Bob to 10 in the second_score
-UPDATE second_score SET score='10' WHERE name='Bob';
+-- update the score of Bob to 10 in the second_table
+UPDATE second_table SET score='10' WHERE name='Bob';
