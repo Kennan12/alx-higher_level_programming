b5f68e12fe44032bd5da87f85722b332657c47d0
kennan12 <kenanusheta24@gmail.com>
2023-11-14 07:45:41 -0800

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
 Exports.nbOccurences = function (list, searchElement) {
-	return (list,filter(e => e === searchElement).length);
+	return list.reduce((count, item) =>
+		(item === searchElement ? count +1 : count), 0);
 };
