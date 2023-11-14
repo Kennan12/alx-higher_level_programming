2fc8e1736fac855d58cfe18e85cf6a87b6cbd7ae
kennan12 <kenanusheta24@gmail.com>
2023-11-14 07:20:54 -0800

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
+
+module.exports = class Rectangle {
 	constructor (w, h) {
 		if (w > 0 && h > 0) {
 			this.width = w;
@@ -15,15 +16,11 @@ class Rectangle {
 	}
 
 	rotate () {
-		const aux = this.width;
-		this.width = this.height;
-		this.height = aux;
+		[this.width, this.height] = [this.height, this.width];
 	}
 
 	double () {
-		this.widht *= 2;
+		this.width *= 2;
 		this.height *= 2;
 	}
-}
-
-module.exports = Rectangle;
+};
