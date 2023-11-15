93d1920851b879b7aa39c903a2df3a0a54086f13
kennan12 <kenanusheta24@gmail.com>
2023-11-15 13:12:19 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,8 @@
 
 if (isNaN(process.argv[2])) {
 	console.log('Missing size');
-}
-else {
-	for (let i = 0; i < parseInt(process.argv[2]); i++)
-	{
-		console.log('X'.repeat(parseInt(process.argv[2]));
+} else {
+	for (let i = 0; i < parseInt(process.argv[2]); i++) {
+		console.log('X'.repeat(parseInt(process.argv[2])));
 	}
 }
