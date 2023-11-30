17b7509ac18befffc58b1d3793cdf2e1e2cd8e9b
kennan12 <kenanusheta24@gmail.com>
2023-11-30 11:07:04 -0800

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # PUT METHOD is allowed, we must follow redirection, we should be user_id = 98
-curl -sLX PUT -H "origin: You got me!" -d "user id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "You got me!" -d "user id=98" 0.0.0.0:5000/catch_me
