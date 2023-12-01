e88c3dd44f5e87a942232903ceea6be73221c84d
kennan12 <kenanusheta24@gmail.com>
2023-12-01 10:55:07 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,10 +3,10 @@
 same as task 0 but using request package
 """
 
-import request
+import requests
 
 if __name__ == "__main__":
-    response = request.get('https://intranet.hbtn.io/status')
+    response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: ()". {}".format(response.text))
+    print("\t- content: {}".format(response.text))
