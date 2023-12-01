ba4eeef0b652bd4806f6e5532b9c37893cb76e6e
kennan12 <kenanusheta24@gmail.com>
2023-12-01 11:36:53 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Fecth
-https://internat.hbtn.io/status 
+"""Fecth https://intranet.hbtn.io/status 
 using urlib package
 """
 
 import urllib.request
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
         response = request.read()
         print("Body response:")
