d0112aec80692389f59d61905c1e1014d661f72b
kennan12 <kenanusheta24@gmail.com>
2024-01-03 07:06:38 -0800

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,8 @@
  */
 const myArgs = process.argv.slice(2);
 const request = require('request');
-request(myArgs[0], function(error, response, body) {
-	if (!error && response)
-	{
+request(myArgs[0], function (error, response, body) {
+	if (!error && response) {
 		console.log('code:', response.statuscode);
 	}
 });
